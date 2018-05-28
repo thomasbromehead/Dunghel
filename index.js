1c2249c8219ccfbddae9d7f9d43b219438340d6c
ThomasBromehead <t_bromehead@yahoo.fr>
2018-05-28 12:59:57 +0200

    Added button to retrieve browser coordinates

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-if(navigator.geolocation){
-   navigator.geolocation.getCurrentPosition(function(position){
-         $('.geoloc').html("latitude: " + position.coords.latitude + "<br/>" + "longitude: " + position.coords.longitude);
-      });
-}
+
+$('#Geo').on('click', function(){
+   if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function(position){
+            $('.geoloc').html("latitude: " + position.coords.latitude + "<br/>" + "longitude: " + position.coords.longitude);
+         });
+   }
+
+})
+
